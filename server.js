718b079dc8f2a1724926c280868c0570a5a97544
Gabriel Borchardt <gabriel.borchardt@millpar.com>
2024-10-20 00:34:21 -0300

    init commit 2

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
 const express = require('express');
 const cors = require('cors');
-const pgp = require('pg-promise')();
+const { Pool } = require('pg');
 require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const db = pgp({
+const pool = new Pool({
 	user: process.env.PGUSER,
 	host: process.env.PGHOST,
 	database: process.env.PGDATABASE,
 	password: process.env.PGPASSWORD,
 	port: process.env.PGPORT,
+	ssl: {
+		rejectUnauthorized: false, // Pode ser útil em alguns ambientes, mas use com cautela em produção
+	},
 });
 
+// Rota para criar um novo usuário
 app.post('/api/users', async (req, res) => {
 	const { name, email } = req.body;
 	try {
-		const result = await db.one('INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *', [name, email]);
-		res.status(201).json(result);
+		const result = await pool.query('INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *', [name, email]);
+		res.status(201).json(result.rows[0]);
 	} catch (err) {
-		console.error('Erro ao conectar:', err.message);
+		console.error(err.message);
 		res.status(500).json({ error: 'Database error', details: err.message });
 	}
 });
@@ -29,10 +33,10 @@ app.post('/api/users', async (req, res) => {
 // Teste de conexão
 app.get('/api/test-connection', async (req, res) => {
 	try {
-		const result = await db.one('SELECT NOW()');
-		res.json(result);
+		const result = await pool.query('SELECT NOW()');
+		res.json(result.rows[0]);
 	} catch (err) {
-		console.error('Erro ao conectar:', err.message);
+		console.error(err.message);
 		res.status(500).json({ error: 'Connection error', details: err.message });
 	}
 });
